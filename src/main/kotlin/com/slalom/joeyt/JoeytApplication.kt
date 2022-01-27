dcece5af4e203a56707e1ad3e68e9212db63dfed
joeyt <joeyt@slalom.com>
2022-01-26 16:28:32 -0800

    spring initializer

---
+++ src/main/kotlin/com/slalom/joeyt/JoeytApplication.kt
@@ -0,0 +1,11 @@
+package com.slalom.joeyt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoeytApplication
+
+fun main(args: Array<String>) {
+	runApplication<JoeytApplication>(*args)
+}
---
+++ src/test/kotlin/com/slalom/joeyt/JoeytApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slalom.joeyt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JoeytApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
