131f4e101772348f5c568b8c84370cead846cc02
joeyslalom <joeyt@slalom.com>
2022-01-26 16:42:41 -0800

    fix my imports

--- src/main/kotlin/com/slalom/joeyt/Demo.kt
+++ src/main/kotlin/com/slalom/joeyt/Demo.kt
@@ -1,5 +1,8 @@
 package com.slalom.joeyt
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 data class Car(val model: String, val make: String)
 
 @RestController
