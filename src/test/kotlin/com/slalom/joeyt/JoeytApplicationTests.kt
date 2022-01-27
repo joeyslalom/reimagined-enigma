976f242fb3409728ef6a4f50f80ef25a21e6cb72
joeyslalom <joeyt@slalom.com>
2022-01-26 16:56:43 -0800

    Try adding a test

--- src/test/kotlin/com/slalom/joeyt/JoeytApplicationTests.kt
+++ src/test/kotlin/com/slalom/joeyt/JoeytApplicationTests.kt
@@ -1,6 +1,7 @@
 package com.slalom.joeyt
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -10,4 +11,9 @@ class JoeytApplicationTests {
 	fun contextLoads() {
 	}
 
+	@Test
+	fun shouldReturn3Cars() {
+		val controller = CarController()
+		assertEquals(controller.cars().length(), 3)
+	}
 }
